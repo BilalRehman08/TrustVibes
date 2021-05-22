2b868a750364c275508170790fbadc81ce2d9ce7
BilalRehman08 <bilalrehman080808@gmail.com>
2021-05-22 13:25:09 -0700

    details screen

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -10,89 +10,126 @@ class _FeedState extends State<Feed> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0, right: 10, top: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  child: CircleAvatar(
-                    radius: 40.0,
-                    backgroundColor: Colors.purple,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: CircleAvatar(
+                      radius: 40.0,
+                      backgroundColor: Colors.purple,
+                    ),
                   ),
-                ),
-                SizedBox(width: 5),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "21",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.004,
-                    ),
-                    Text(
-                      "Posts",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
+                  SizedBox(width: 5),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "21",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "21",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.004,
-                    ),
-                    Text(
-                      "Followers",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.004,
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "21",
-                      style: TextStyle(
+                      Text(
+                        "Posts",
+                        style: TextStyle(
                           color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.004,
-                    ),
-                    Text(
-                      "Following",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "21",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.004,
+                      ),
+                      Text(
+                        "Followers",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "21",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.004,
+                      ),
+                      Text(
+                        "Following",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          )
-        ]),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              "Diniharistd",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
+            ),
+            Text(
+              "@portugal",
+              style: TextStyle(
+                color: Colors.purple,
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.008,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 60.0),
+              child: Text(
+                "Diniharistd is a upcoming sci-fi and fantasy author, artist, photojournalist and musician.",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
