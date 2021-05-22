3ebd0558eac92d3ffc69ecc8fe63d9d09e6f7949
BilalRehman08 <bilalrehman080808@gmail.com>
2021-05-21 17:12:42 -0700

    details screen

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -8,48 +8,55 @@ class Details extends StatefulWidget {
 class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-      child: Stack(children: <Widget>[
-        Container(
-          color: Colors.green,
-          height: MediaQuery.of(context).size.height * 0.30,
-          width: MediaQuery.of(context).size.width * 1,
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 50),
-          child: Column(children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.notification_add,
+    return MaterialApp(
+      home: Scaffold(
+          body: SingleChildScrollView(
+        child: Stack(children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.30,
+            width: MediaQuery.of(context).size.width * 1,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/back.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 50),
+            child: Column(children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.notification_add,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      // Navigator.push(context,
+                      //     MaterialPageRoute(builder: (context) => MyApp()));
+                    },
+                  ),
+                  Text('Diniharistd',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      )),
+                  Icon(
+                    Icons.menu,
                     size: 30,
                     color: Colors.white,
                   ),
-                  onPressed: () {
-                    // Navigator.push(context,
-                    //     MaterialPageRoute(builder: (context) => MyApp()));
-                  },
-                ),
-                Text('Diniharistd',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    )),
-                Icon(
-                  Icons.menu,
-                  size: 30,
-                  color: Colors.white,
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Text("Continue....."),
-          ]),
-        ),
-      ]),
-    ));
+                ],
+              ),
+              SizedBox(height: 30),
+              Text("Continue....."),
+            ]),
+          ),
+        ]),
+      )),
+    );
   }
 }
